c0c1f0aa36d94710ad4627aed4153768bb56cdb8
Sami Kerola <kerolasa@iki.fi>
2013-12-28 15:36:00 +0000

    use gnulib to print --help and --version
    * Makefile.am: update help2man invocation and add MAINTAINERCLEANFILES
    * bootstrap.conf: include dirname, propername, and version-etc-fsf modules
    * configure.ac: remove COPYRIGHT_YEAR from config.h
    * doc/hello.texi: remove short options -h and -v
    * po/POTFILES.in: add lib/version-etc.c to translations
    * src/hello.c: remove print_version(), and rename print_help() to usage()
    * src/system.h: add inline functions to print version and usage info

--- src/hello.c
+++ src/hello.c
@@ -19,25 +19,31 @@
 #include <config.h>
 #include "system.h"
 #include "progname.h"
+#include "propername.h"
 #include "xalloc.h"
+
+/* The official name of this program (e.g., no `g' prefix).  */
+#define PROGRAM_NAME "hello"
+
+#define AUTHORS \
+  proper_name ("The King"), \
+  proper_name ("others")
 
 static const struct option longopts[] = {
   {"greeting", required_argument, NULL, 'g'},
-  {"help", no_argument, NULL, 'h'},
   {"traditional", no_argument, NULL, 't'},
-  {"version", no_argument, NULL, 'v'},
+  {GETOPT_HELP_OPTION_DECL},
+  {GETOPT_VERSION_OPTION_DECL},
   {NULL, 0, NULL, 0}
 };
 
 /* Forward declarations.  */
-static void print_help (void);
-static void print_version (void);
+static void usage (int status);
 
 int
 main (int argc, char *argv[])
 {
   int optc;
-  int lose = 0;
   const char *greeting_msg;
   wchar_t *mb_greeting;
   size_t len;
@@ -62,38 +68,28 @@ main (int argc, char *argv[])
      This is implemented in the Gnulib module "closeout".  */
   atexit (close_stdout);
 
-  while ((optc = getopt_long (argc, argv, "g:htv", longopts, NULL)) != -1)
+  while ((optc = getopt_long (argc, argv, "g:t", longopts, NULL)) != -1)
     switch (optc)
       {
-	/* --help and --version exit immediately, per GNU coding standards.  */
-      case 'v':
-	print_version ();
-	exit (EXIT_SUCCESS);
-	break;
       case 'g':
 	greeting_msg = optarg;
-	break;
-      case 'h':
-	print_help ();
-	exit (EXIT_SUCCESS);
 	break;
       case 't':
 	greeting_msg = _("hello, world");
 	break;
+      /* --help and --version exit immediately, per GNU coding standards.  */
+      case_GETOPT_HELP_CHAR;
+      case_GETOPT_VERSION_CHAR (PROGRAM_NAME, AUTHORS);
       default:
-	lose = 1;
-	break;
+	usage (EXIT_FAILURE);
       }
 
-  if (lose || optind < argc)
+  if (optind < argc)
     {
       /* Print error message and exit.  */
-      if (optind < argc)
-	fprintf (stderr, _("%s: extra operand: %s\n"), program_name,
-		 argv[optind]);
-      fprintf (stderr, _("Try `%s --help' for more information.\n"),
-	       program_name);
-      exit (EXIT_FAILURE);
+      fprintf (stderr, _("%s: extra operand: %s\n"), program_name,
+               argv[optind]);
+      usage (EXIT_FAILURE);
     }
 
   len = mbsrtowcs(NULL, &greeting_msg, 0, NULL);
@@ -118,71 +114,34 @@ main (int argc, char *argv[])
    blocks and identify the various pieces.  */
 
 static void
-print_help (void)
+usage (int status)
 {
-  /* TRANSLATORS: --help output 1 (synopsis)
-     no-wrap */
-  printf (_("\
-Usage: %s [OPTION]...\n"), program_name);
+  if (status != EXIT_SUCCESS)
+    emit_try_help ();
+  else
+    {
+      /* TRANSLATORS: --help output 1 (synopsis)
+         no-wrap */
+      printf (_("\
+Usage: %s OPTION...\n"), program_name);
 
-  /* TRANSLATORS: --help output 2 (brief description)
-     no-wrap */
-  fputs (_("\
+      /* TRANSLATORS: --help output 2 (brief description)
+         no-wrap */
+      fputs (_("\
 Print a friendly, customizable greeting.\n"), stdout);
 
-  puts ("");
-  /* TRANSLATORS: --help output 3: options 1/2
-     no-wrap */
-  fputs (_("\
-  -h, --help          display this help and exit\n\
-  -v, --version       display version information and exit\n"), stdout);
+      emit_mandatory_arg_note ();
 
-  puts ("");
-  /* TRANSLATORS: --help output 4: options 2/2
-     no-wrap */
-  fputs (_("\
+      /* TRANSLATORS: --help output 3: options 2/2
+         no-wrap */
+      fputs (_("\
   -t, --traditional       use traditional greeting\n\
   -g, --greeting=TEXT     use TEXT as the greeting message\n"), stdout);
 
-  printf ("\n");
-  /* TRANSLATORS: --help output 5+ (reports)
-     TRANSLATORS: the placeholder indicates the bug-reporting address
-     for this application.  Please add _another line_ with the
-     address for translation bugs.
-     no-wrap */
-  printf (_("\
-Report bugs to: %s\n"), PACKAGE_BUGREPORT);
-#ifdef PACKAGE_PACKAGER_BUG_REPORTS
-  printf (_("Report %s bugs to: %s\n"), PACKAGE_PACKAGER,
-	  PACKAGE_PACKAGER_BUG_REPORTS);
-#endif
-#ifdef PACKAGE_URL
-  printf (_("%s home page: <%s>\n"), PACKAGE_NAME, PACKAGE_URL);
-#else
-  printf (_("%s home page: <http://www.gnu.org/software/%s/>\n"),
-	  PACKAGE_NAME, PACKAGE);
-#endif
-  fputs (_("General help using GNU software: <http://www.gnu.org/gethelp/>\n"),
-	 stdout);
-}
-
+      fputs (HELP_OPTION_DESCRIPTION, stdout);
+      fputs (VERSION_OPTION_DESCRIPTION, stdout);
 
-
-/* Print version and copyright information.  */
-
-static void
-print_version (void)
-{
-  printf ("%s (%s) %s\n", PACKAGE, PACKAGE_NAME, VERSION);
-  /* xgettext: no-wrap */
-  puts ("");
-
-  /* It is important to separate the year from the rest of the message,
-     as done here, to avoid having to retranslate the message when a new
-     year comes around.  */
-  printf (_("\
-Copyright (C) %d Free Software Foundation, Inc.\n\
-License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\
-This is free software: you are free to change and redistribute it.\n\
-There is NO WARRANTY, to the extent permitted by law.\n"), COPYRIGHT_YEAR);
+      emit_ancillary_info ();
+    }
+  exit (status);
 }
--- src/system.h
+++ src/system.h
@@ -33,6 +33,9 @@
 #include <wchar.h>
 #include <wctype.h>
 
+/* CHAR_MIN */
+#include <limits.h>
+
 /* Internationalization.  */
 # include "gettext.h"
 # define _(str) gettext (str)
@@ -40,5 +43,92 @@
 
 /* Check for errors on write.  */
 # include "closeout.h"
+
+/* Usage and version priting.  */
+# include "dirname.h"
+# include "progname.h"
+
+/* These enum values cannot possibly conflict with the option values
+   ordinarily used by commands, including CHAR_MAX + 1, etc.  Avoid
+   CHAR_MIN - 1, as it may equal -1, the getopt end-of-options value.  */
+enum
+{
+  GETOPT_HELP_CHAR = (CHAR_MIN - 2),
+  GETOPT_VERSION_CHAR = (CHAR_MIN - 3)
+};
+
+#define GETOPT_HELP_OPTION_DECL \
+  "help", no_argument, NULL, GETOPT_HELP_CHAR
+#define GETOPT_VERSION_OPTION_DECL \
+  "version", no_argument, NULL, GETOPT_VERSION_CHAR
+
+#define case_GETOPT_HELP_CHAR                   \
+  case GETOPT_HELP_CHAR:                        \
+    usage (EXIT_SUCCESS);                       \
+    break;
+
+#define emit_bug_reporting_address unused__emit_bug_reporting_address
+#include "version-etc.h"
+#undef emit_bug_reporting_address
+
+#include "propername.h"
+/* Define away proper_name (leaving proper_name_utf8, which affects far
+   fewer programs), since it's not worth the cost of adding ~17KB to
+   the x86_64 text size of every single program.  This avoids a 40%
+   (almost ~2MB) increase in the on-disk space utilization for the set
+   of the 100 binaries. */
+#define proper_name(x) (x)
+
+#define case_GETOPT_VERSION_CHAR(Program_name, Authors)                 \
+  case GETOPT_VERSION_CHAR:                                             \
+    version_etc (stdout, Program_name, PACKAGE_NAME, VERSION, Authors,  \
+                 (char *) NULL);                                        \
+    exit (EXIT_SUCCESS);                                                \
+    break;
+
+#define HELP_OPTION_DESCRIPTION \
+  _("      --help     display this help and exit\n")
+#define VERSION_OPTION_DESCRIPTION \
+  _("      --version  output version information and exit\n")
+
+static inline void
+emit_mandatory_arg_note (void)
+{
+  fputs (_("\n\
+Mandatory arguments to long options are mandatory for short options too.\n\
+"), stdout);
+}
+
+/* Just like strncmp, but the second argument must be a literal string
+   and you don't specify the length;  that comes from the literal.  */
+#define STRNCMP_LIT(s, literal) \
+  strncmp (s, "" literal "", sizeof (literal) - 1)
+
+static inline void
+emit_ancillary_info (void)
+{
+  printf (_("\n%s online help: <%s>\n"), PACKAGE_NAME, PACKAGE_URL);
+  /* Don't output this redundant message for English locales.
+     Note we still output for 'C' so that it gets included in the man page.  */
+  const char *lc_messages = setlocale (LC_MESSAGES, NULL);
+  if (lc_messages && STRNCMP_LIT (lc_messages, "en_"))
+    {
+      /* TRANSLATORS: Replace LANG_CODE in this URL with your language code
+         <http://translationproject.org/team/LANG_CODE.html> to form one of
+         the URLs at http://translationproject.org/team/.  Otherwise, replace
+         the entire URL with your translation team's email address.  */
+      printf (_("Report %s translation bugs to "
+                "<http://translationproject.org/team/>\n"),
+                last_component (program_name));
+    }
+  printf (_("For complete documentation, run: "
+            "info %s '%s invocation'\n"), PACKAGE, last_component (program_name));
+}
+
+static inline void
+emit_try_help (void)
+{
+  fprintf (stderr, _("Try '%s --help' for more information.\n"), program_name);
+}
 
 #endif /* HELLO_SYSTEM_H */
